e65da7d1ed0b75e3e62ada18f4b9e802cead5008
xLJohnsonx <lizjohnsonemail@gmail.com>
2018-09-15 21:33:47 -0700

    word count with basic hash

--- 04-word-count/word_count.rb
+++ 04-word-count/word_count.rb
@@ -1,1 +1,19 @@
 
+# word count
+def words(words)
+  # split srting of words into an array
+  words = words.split(' ')
+  word_hsh = {}
+  # iterate over each word
+  words.each do |word|
+     # create new k,v pair unless word already exists as key
+     # in which case increment the v by 1
+    if word_hsh.key? word
+      word_hsh[word] += 1
+    else
+      word_hsh[word] = 1
+    end
+  end
+
+  return word_hsh
+end
