753054341bac2443a508577a5b47b33e86a69841
xLJohnsonx <lizjohnsonemail@gmail.com>
2018-09-15 22:09:22 -0700

    DRYed up original method

--- 04-word-count/word_count.rb
+++ 04-word-count/word_count.rb
@@ -1,19 +1,25 @@
 
 # word count
+# split string of words into an array of words by ' '
+# create hash with default value of 0 to avoid undefined method + for nil
+# e.g. hash.new => nil is default value
+# iterate over each word
+# create new k,v pair unless word already exists as key
+# in which case increment the v by 1
 def words(words)
-  # split srting of words into an array
   words = words.split(' ')
-  word_hsh = {}
-  # iterate over each word
+  word_hsh = Hash.new(0)
   words.each do |word|
-     # create new k,v pair unless word already exists as key
-     # in which case increment the v by 1
-    if word_hsh.key? word
-      word_hsh[word] += 1
-    else
-      word_hsh[word] = 1
-    end
+    word_hsh[word] += 1
   end
 
   return word_hsh
 end
+
+# def words(words)
+#   words = words.split(' ')
+#   word_hsh = words.each_with_object({}) do |word, hash|
+#     hash[word] += 1
+#   end
+#   return word_hsh
+# end
